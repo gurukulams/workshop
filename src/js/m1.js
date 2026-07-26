5f65a58daa76468e1ecd326ac6f02fcbb7e644a3
sathishk <sathishkumar.thiyagarajan@gmail.com>
2026-07-26 12:04:23 +0530

    Workshop

--- src/js/m1.js
+++ src/js/m1.js
@@ -1,5 +1,10 @@
 import QuestionLoader from '../../site/themes/design-system/src/js/components/QuestionLoader'
+const hashValue = window.location.hash.slice(1); 
+console.log(hashValue);
+const element = document.querySelector("#practice-container");
 
+// Set the attribute
+element.dataset.practiceCategory = hashValue;
 new QuestionLoader();
 
 const message = "Hello from Module 1";
